9b19dda3133eb9446519f5d7b66a84e2229f90ee
Eden <enegussie@fh.org>
2025-02-05 19:25:26 +0300

    get mock data from db

--- server.js
+++ server.js
@@ -2,13 +2,51 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+require('dotenv').config();
 
 // Middleware
 app.use(express.json());
 
+
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
+});
+
+
 // Sample Route
 app.get("/", (req, res) => {
     res.json({ message: "Served!" });
+});
+
+// Get all tasks
+app.get('/tasks', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM task');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving tasks');
+    }
+});
+
+// Get task by ID
+app.get('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query('SELECT * FROM task WHERE id = $1', [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).send('Task not found');
+        }
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving task');
+    }
 });
 
 // Start Server
